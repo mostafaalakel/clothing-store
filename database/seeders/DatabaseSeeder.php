29c1fade0addd867271e8b5ef8bd6f0d2619f254
mostafa akel <mostafaalakel947@gmail.com>
2024-10-27 20:05:21 +0300

    add ProductDiscountSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
             ReviewSeeder::class,
             DiscountSeeder::class,
             DiscountCodeSeeder::class,
+            ProductDiscountSeeder::class
         ]);
     }
 }
--- database/seeders/ProductDiscountSeeder.php
+++ database/seeders/ProductDiscountSeeder.php
@@ -18,13 +18,11 @@ class ProductDiscountSeeder extends Seeder
 
         foreach ($productIds as $productId) {
             $randomDiscounts = array_rand($discountIds, rand(1, count($discountIds)));
-            
+
             foreach ((array) $randomDiscounts as $discountIndex) {
                 DB::table('product_discounts')->insert([
                     'product_id' => $productId,
-                    'discount_id' => $discountIds[$discountIndex],
-                    'created_at' => now(),
-                    'updated_at' => now(),
+                    'discount_id' => $discountIds[$discountIndex]
                 ]);
             }
         }
