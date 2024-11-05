5c8693de2ef0ad6cfa3fe56ea7ef339f979c4c9f
mostafa akel <mostafaalakel947@gmail.com>
2024-11-05 16:33:22 +0300

    add product and review resources

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'gender' => $this->gender, 
+            'main_image' => $this->productImages->first()->image_url,
+            'price' => $this->price,
+            'avg_rating' => $this->avg_rating,
+            'reviews' => ReviewResource::collection($this->reviews)
+        ];
+    }
+}
