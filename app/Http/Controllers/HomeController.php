ba661a6f1779c08aa53bcdccbff608259d8acd24
mostafa akel <mostafaalakel947@gmail.com>
2024-11-05 17:48:49 +0300

    add index fn to ProductController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\ProductResource;
+use App\Http\Traits\ApiResponseTrait;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -11,10 +12,11 @@ use PhpParser\Node\Stmt\Return_;
 
 class HomeController extends Controller
 {
+    use ApiResponseTrait;
     public function index()
     {
-        $products = Product::get()->sortByDesc('avg_rating');
+        $products = Product::get()->sortByDesc('avg_rating')->take(15);
         $products = ProductResource::collection($products);
-        return $products;
+        return $this->retrievedResponse($products , 'home page retrieved successfully');
     }
 }
