4116987b1471a9f010b84551c2a402a5cd637c71
mostafa akel <mostafaalakel947@gmail.com>
2024-11-07 21:49:41 +0300

    update reviewseeder

--- database/seeders/ReviewSeeder.php
+++ database/seeders/ReviewSeeder.php
@@ -18,13 +18,11 @@ class ReviewSeeder extends Seeder
                 'product_id' => 1,
                 'user_id' => 1,
                 'rating' => 5,
-                'comments' => 'Excellent product!'
             ],
             [
                 'product_id' => 2,
                 'user_id' => 1,
                 'rating' => 4,
-                'comments' => 'Very good shoes.'
             ],
         ]);
     }
